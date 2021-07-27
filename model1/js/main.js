d0c07b054ab2f34978a5ff2d4b691db345be0a2a
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-27 19:36:08 -0300

    inicio js

---
+++ model1/js/main.js
@@ -0,0 +1,8 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var msg = window.document.getElementById('foto')
+    var data = new.Date()
+    var hora = data.getHours()
+
+}
+
