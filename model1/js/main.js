6baa50e4a047419e87a3a80cf7286a3e541f01dd
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-27 19:46:30 -0300

    .

--- model1/js/main.js
+++ model1/js/main.js
@@ -1,8 +1,18 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var msg = window.document.getElementById('foto')
-    var data = new.Date()
+function carregar(){
+    var msg = window.document.getElementById("msg")
+    var foto = window.document.getElementById('foto')
+    var data = new Date()
     var hora = data.getHours()
-
+    msg.innerHTML= `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        // Bom diaaa
+        img.src = 'img/foto_manha.png'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tardee
+        img.src = 'img/foto_tarde.png'
+    } else{
+        // Boa noitee
+        img.src = 'img/foto_noite.png'
+    }
+    
 }
-
