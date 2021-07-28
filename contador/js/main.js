14538d365beded28dd18d6c5111d6727fa94217c
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-28 09:30:32 -0300

    Update main.js

--- contador/js/main.js
+++ contador/js/main.js
@@ -1,15 +1,21 @@
 function contar(){
     var inicio = document.getElementById('txtinicio')
-    num_inicio = Number(inicio.value);
     var fim = document.getElementById('txtfinal')
-    num_final = Number(final.value);
     var passos = document.getElementById('txtpassos')
-    num_passos = Number(passos.value);
+    var res = document.getElementById('res')
 
-    if(inicio.value.lenth == 0 || fim.value.lenth == 0 || passos.value.lenth == 0){
+    if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0){
         window.alert('ERRO! Dados estão comprometidos')
     } else{
-        alert('Td ok!')
+        res.innerHTML = ('Contando: ')
+        let inicio_num = Number(inicio.value)
+        let fim_num = Number(fim.value)
+        let passsos_num = Number(passos.value)
+
+        for(var c = 1; c <= fim_num ; c += p){
+            res.innerHTML += `${c} `
+        }
+
     }
 
 }
