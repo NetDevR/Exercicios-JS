3304544f2120a735b7f6a6e07498a75e13b95249
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-28 09:47:02 -0300

    Update main.js

--- contador/js/main.js
+++ contador/js/main.js
@@ -6,16 +6,23 @@ function contar(){
 
     if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0){
         window.alert('ERRO! Dados estão comprometidos')
+        res.innerHTML('Impossivel contar')
     } else{
-        res.innerHTML = ('Contando: ')
+        res.innerHTML = ('Contando: <br>')
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passos.value)
+        if (p <= 0){
+            window.alert('Passo inválido. Considerando passo 1')
+            p = 1
+        }
         if(i < f){
+            //Contagem Crescente
             for( let c = 1; c <= f; c += p){
                 res.innerHTML += `${c} \u{1F449}`
             } 
         }else{
+            //Contagem Decrescente
             for( let c = i; c >= f; c -= p){
                 res.innerHTML += `${c} \u{1F449}`
             }
