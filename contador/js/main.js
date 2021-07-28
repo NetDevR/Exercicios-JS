844607d48eef40f3e900443f82bd86fda6fb90b7
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-28 09:43:44 -0300

    contador de números com interface web funcional

--- contador/js/main.js
+++ contador/js/main.js
@@ -11,12 +11,18 @@ function contar(){
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passos.value)
+        if(i < f){
+            for( let c = 1; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            } 
+        }else{
+            for( let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
 
-        for( let c = 1; c <= f; c += p){
-            res.innerHTML += `${c} `
         }
+        res.innerHTML += `\u{1F6A9}`
 
     }
-
 }
-
